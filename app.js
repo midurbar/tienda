343b2ed07fe140505ab5ac656e70dc0ac86959c1
afd <afd@alup5.academiapostal.es.local>
2020-02-26 20:41:16 +0100

    incluir base de datos

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var cookieSession = require('cookie-session');
 var logger = require('morgan');
+const models=require('./models');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
