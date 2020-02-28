47c1c8b39a2f347bf403bc163bcc4b4695e79bfd
afd <afd@alup5.academiapostal.es.local>
2020-02-28 19:32:11 +0100

    camb usuario

--- models/users.js
+++ models/users.js
@@ -3,10 +3,10 @@ const sequelize= require('./db');
 
 //definimos el modelo para usuario
 const Usuario=sequelize.define('usuario',{
-    email:Sequelize.STRING(50),
-    password:Sequelize.STRING(80),
-    nombre:Sequelize.STRING,
-    apellidos:Sequelize.STRING
+    email:{type: Sequelize.STRING(100),allowNull:false, unique:true},
+    password:Sequelize.STRING(40),
+    nombre:Sequelize.STRING(50),
+    apellidos:Sequelize.STRING(80)
 });
 
 module.exports=Usuario;
