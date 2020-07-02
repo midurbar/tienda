1d7c68829e8256ddcf7a498a26105a1b63fea1d7
midurbar <60234563+midurbar@users.noreply.github.com>
2020-07-02 17:54:49 +0200

    Update users.js
    corregir

--- models/users.js
+++ models/users.js
@@ -6,7 +6,7 @@ const Usuario=sequelize.define('usuario',{
     email:{type: Sequelize.STRING(100),allowNull:false, unique:true},
     password:{type: Sequelize.STRING(40),allowNull:false},
     nombre:{type: Sequelize.STRING(50),allowNull:false},
-    apellidos:{type:Sequelize.STRING(80),allowNull:false}
+    apellidos:{type: Sequelize.STRING(80),allowNull:false}
 });
 
 module.exports=Usuario;
