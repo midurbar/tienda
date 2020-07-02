9c216835fa4f400df865e7ad61fec8ad0af30a6b
midurbar <60234563+midurbar@users.noreply.github.com>
2020-07-02 17:54:17 +0200

    Update users.js
    camb usuario

--- models/users.js
+++ models/users.js
@@ -4,9 +4,9 @@ const sequelize= require('./db');
 //definimos el modelo para usuario
 const Usuario=sequelize.define('usuario',{
     email:{type: Sequelize.STRING(100),allowNull:false, unique:true},
-    password:Sequelize.STRING(40),
-    nombre:Sequelize.STRING(50),
-    apellidos:Sequelize.STRING(80)
+    password:{type: Sequelize.STRING(40),allowNull:false},
+    nombre:{type: Sequelize.STRING(50),allowNull:false},
+    apellidos:{type:Sequelize.STRING(80),allowNull:false}
 });
 
 module.exports=Usuario;
